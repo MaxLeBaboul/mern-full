3788292a84be448ba8a1281ffe8b49834bfc5a73
Lee Maxwell <maxwellbaboula@gmail.com>
2022-09-04 05:02:42 +0100

    client App add whith react

---
+++ backend/server.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dotenv = require("dotenv")
+const app = express()
+const port = 4000
+
+
+app.use(express.json());
+dotenv.config()
+
+
+//data base connect
+
+
+const uri = process.env.STRING_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+app.get("/", (req, res) =>{
+    client.connect((err, db) => {
+      console.log("Database connected");
+      if(err ||!db){return false}
+        db.db("blog").collection("posts").find().toArray((err, results) => {
+            if(!err){
+                res.status(200).send(results)
+            }
+        })
+      // perform actions on the collection object
+    //   client.close();
+    });
+})
+
+
+app.post("/insert", (req, res) => {
+  client.connect((err, db) => {
+    console.log("Database connected");
+    if (err || !db) {
+      return false;
+    }
+    db.db("blog")
+      .collection("posts")
+      .insertOne(req.body, (err, results) => {
+        if (!err) {
+          res.status(200).send(results);
+        }
+      })
+      
+    // perform actions on the collection object
+    //   client.close();
+  });
+});
+
+
+
+app.listen(port, () =>{
+    console.log(`Server démarré avec succès sur le port ${port}`);
+})
