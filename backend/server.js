860ce8593986eb55ae2cc19ea25fec3696080b1d
Lee Maxwell <maxwellbaboula@gmail.com>
2022-09-13 14:10:15 +0100

    application build in Backend

--- backend/server.js
+++ backend/server.js
@@ -1,60 +1,21 @@
-const express = require("express")
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const dotenv = require("dotenv")
-const cors = require("cors")
-const app = express()
-const port = 4000
+import express from "express";
+import cors from "cors";
+import path from "path";
+const app = express();
+const routes = express.Router();
+const port = 4000;
 
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-dotenv.config()
+app.use("/posts", routes);
+require("./routes")(routes);
 
-
-//data base connect
-
-
-const uri = process.env.STRING_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-
-app.get("/", (req, res) =>{
-    client.connect((err, db) => {
-      console.log("Database connected");
-      if(err ||!db){return false}
-        db.db("blog").collection("posts").find().toArray((err, results) => {
-            if(!err){
-                res.status(200).send(results)
-            }
-        })
-      // perform actions on the collection object
-    //   client.close();
-    });
-})
-
-
-app.post("/insert", (req, res) => {
-  client.connect((err, db) => {
-    console.log("Database connected");
-    if (err || !db) {
-      return false;
-    }
-    db.db("blog")
-      .collection("posts")
-      .insertOne(req.body, (err, results) => {
-        if (!err) {
-          res.status(200).send(results);
-        }
-      })
-      
-    // perform actions on the collection object
-    //   client.close();
-  });
+const public_path = path.join(__dirname, "./build");
+app.use(express.static(public_path));
+app.get("*", (_, res) => {
+  res.sendFile(path.join(public_path, "index.html"));
 });
 
-
-
-app.listen(port, () =>{
-    console.log(`Server démarré avec succès sur le port ${port}`);
-})
+app.listen(port, () => {
+  console.log("serveur démarré avec succès sur le port 4000");
+});
